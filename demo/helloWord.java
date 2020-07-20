0ef1b4d49fe3fd921c84e193246bf5972d1a97fe
angle-99 <1971561926@qq.com>
2020-07-20 14:01:26 +0800

    Java第一次练习

---
+++ demo/helloWord.java
@@ -0,0 +1,13 @@
+package cn.tx.demo;
+
+
+import java.util.Date;
+
+public class helloWord {
+    public static void main(String[] args) {
+        System.out.println("helloWord");
+
+        Date date=new Date();
+        
+    }
+}
